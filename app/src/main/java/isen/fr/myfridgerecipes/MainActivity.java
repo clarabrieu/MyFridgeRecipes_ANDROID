1d9a7aa74534c9933c3fadeac3b5bdffca2a0ec2
margotGermain <34025134+margotGermain@users.noreply.github.com>
2018-03-20 18:38:25 +0100

    Rajout page MANUAL
    Manque les boutons enter

--- app/src/main/java/isen/fr/myfridgerecipes/MainActivity.java
+++ app/src/main/java/isen/fr/myfridgerecipes/MainActivity.java
@@ -1,17 +1,10 @@
 package isen.fr.myfridgerecipes;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,62 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String DONNEE = "json";
-        String barecode="";
-
-        Intent intent = getIntent();
-        if (intent != null) {
-            try {
-                JSONObject obj = new JSONObject(intent.getStringExtra(DONNEE));
-                barecode=(obj.getString("barecode"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        new MainActivityTask(new CallBackInterface() {
-            @Override
-            public void success(String json) {
-                Gson gson = new GsonBuilder().create();
-                UserResults userResults = gson.fromJson(json, UserResults.class);
-                int status = userResults.getResults().get(0).getStatus();
-
-                String wordEn = null;
-                if (status == 1) {
-                    String generic_name = userResults.getResults().get(0).getProduct().getGeneric_name();
-                    if (generic_name == "" || generic_name == null) {
-                        String product_name = userResults.getResults().get(0).getProduct().gatProduct_name();
-                        //this.produit2 = this.removeAccents(product_name);
-                    } else {
-                        //this.produit2 = this.removeAccents(generic_name);
-                    }
-
-                }
-            }
-
-            @Override
-            public void error() {
-
-            }
-        },barecode).execute();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         // Redirection vers la page FRIDGE
-
         final ImageView fridge = (ImageView) findViewById(R.id.img_fridge);
         fridge.setOnClickListener(new View.OnClickListener() {
 
@@ -85,19 +25,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*
-        // Redirection vers la page MANUAL
-        final ImageView fridge = (ImageView) findViewById(R.id.img_fridge);
-        fridge.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FridgeActivity.class);
-                startActivity(intent);
-            }
-        });*/
-
-
         // Redirection vers la page SCANNER
         final ImageView barcode = (ImageView) findViewById(R.id.img_barcode);
         barcode.setOnClickListener(new View.OnClickListener() {
@@ -105,6 +32,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Redirection vers la page MANUAL
+        final ImageView manual = (ImageView) findViewById(R.id.img_manual);
+        manual.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManualActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/isen/fr/myfridgerecipes/ScannerActivity.java
+++ app/src/main/java/isen/fr/myfridgerecipes/ScannerActivity.java
@@ -12,22 +12,11 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class ScannerActivity extends AppCompatActivity {
 
-    final String DONNEE = "json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_scanner);
 
         final Activity activity = this;
@@ -65,56 +54,9 @@ public class ScannerActivity extends AppCompatActivity {
             } else {
                 Log.d("MainActivity", "Scanned");
                 Toast.makeText(this, "Product scanned : " + result.getContents(), Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(ScannerActivity.this, MainActivityTask.class);
-                writeFile(result.getContents().toString());
-                String data2=readJSON();
-                intent.putExtra(DONNEE, data2);
-                startActivity(intent);
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
-
-    public String readJSON(){
-        String data="";
-        FileInputStream load;
-        try {
-            load=openFileInput("save.json");
-            int size=load.available();
-            byte [] buffer =new byte[size];
-            load.read(buffer);
-            data = new String(buffer);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return data;
-
-    }
-
-    public JSONObject writeJSON(String barecode) {
-        JSONObject save = new JSONObject();
-        try {
-            save.put("barecode",barecode);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return save;
-    }
-    public void writeFile(String barecode) {
-        FileOutputStream file;
-        String save=writeJSON(barecode).toString();
-        try {
-            file=openFileOutput("save.json",MODE_PRIVATE);
-            file.write(save.getBytes());
-            file.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
